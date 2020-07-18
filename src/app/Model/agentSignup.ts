9100b331c16c3c5a0277f2f976dae11ce8e4f2f8
Sanpstreet <68464798+Snapstreet@users.noreply.github.com>
2020-07-18 17:44:34 +0530

    Update agentSignup.ts

--- src/app/Model/agentSignup.ts
+++ src/app/Model/agentSignup.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export class agentSignup {
     key: string;
     fullname: any;
@@ -12,19 +12,4 @@ export class agentSignup {
     multiplefees:any;
     image:any;
    active = true;
-=======
-export class agentSignup {
-    key: string;
-    fullname: any;
-    company:any;
-    postcode:any;
-    address:any
-    jobtitle:any;
-    phone:any;
-    email:any;
-    solefees:any;
-    multiplefees:any;
-    image:any;
-   active = true;
->>>>>>> 651bfc8cf0ef365f0c06fc4dd4780533889dee96
 }
