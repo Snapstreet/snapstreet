3340580c0771802bdbeeb388a81b48cec2e5f20c
Sanpstreet <68464798+Snapstreet@users.noreply.github.com>
2020-07-18 17:43:17 +0530

    Update notification.component.ts

--- src/app/Menu/notification/notification.component.ts
+++ src/app/Menu/notification/notification.component.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Component, OnInit } from '@angular/core';
 import {NotificationService} from './notification.service';
 
@@ -28,34 +28,3 @@ export class NotificationComponent implements OnInit {
   }
 
 }
-=======
-import { Component, OnInit } from '@angular/core';
-import {NotificationService} from './notification.service';
-
-@Component({
-  selector: 'app-notification',
-  templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.css']
-})
-export class NotificationComponent implements OnInit {
-  user: any;
-  uid: any;
-  confirmInterest= [];
-
-  constructor(private NotificationService:NotificationService) { }
-
-  ngOnInit() {
-    this.user = JSON.parse(localStorage.getItem("user"));
-    this.uid = this.user.uid;
-  
-
-    this.NotificationService.getnotifications(this.uid).subscribe((ref) => {
-      ref.forEach((item) => {
-      this.confirmInterest.push(item.data())
-     
-      });
-    });
-  }
-
-}
->>>>>>> 651bfc8cf0ef365f0c06fc4dd4780533889dee96
